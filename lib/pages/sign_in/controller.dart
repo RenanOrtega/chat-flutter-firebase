e40fbe8993715ec2252c430812356252e5bae522
Renan Johny Ortega <renanortega82@gmail.com>
2023-01-19 20:40:39 -0300

    feat: add sign in page

---
+++ lib/pages/sign_in/controller.dart
@@ -0,0 +1,9 @@
+import 'package:chat_flutter_firebase/common/routes/names.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+
+class SignInController extends GetxController {
+  final state = SignInState();
+  SignInController();
+}
