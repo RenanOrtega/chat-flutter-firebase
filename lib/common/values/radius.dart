9b85f1a85d326558bb3270d5770b0376ef9b46e4
Renan Johny Ortega <renanortega82@gmail.com>
2023-01-20 14:33:47 -0300

    feat: create colors and radius values.

---
+++ lib/common/values/radius.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/rendering.dart';
+
+class AppRadius {
+  static const BorderRadiusGeometry k6pxRadius =
+      BorderRadius.all(Radius.circular(20));
+  static const BorderRadiusGeometry k54pxRadius =
+      BorderRadius.all(Radius.circular(54));
+}
--- lib/common/values/values.dart
+++ lib/common/values/values.dart
@@ -2,3 +2,5 @@ library values;
 
 export 'storage.dart';
 export 'shadows.dart';
+export 'colors.dart';
+export 'radius.dart';
